80c97b5043470fcd1a1934e4be10fc1e1981303c
Stephane Peter <megastep@megastep.org>
2000-10-27 23:32:31 +0000

    Bumped up the release number.

--- install.h
+++ install.h
@@ -27,7 +27,7 @@
 
 #define SETUP_VERSION_MAJOR 1
 #define SETUP_VERSION_MINOR 5
-#define SETUP_VERSION_RELEASE 4
+#define SETUP_VERSION_RELEASE 5
 
 /* The different setup states */
 typedef enum {
