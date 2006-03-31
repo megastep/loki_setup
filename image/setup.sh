7bb7cebb1ceb71fd40a07031b95c9be3eba12a97
Stephane Peter <megastep@megastep.org>
2006-03-31 01:28:03 +0000

    Create separate GTK2 binaries (setup.gtk2), and make setup.sh pick it up. Copy loki_uninstall to different directories depending on GTK

--- image/setup.sh
+++ image/setup.sh
@@ -264,16 +264,20 @@ then
   fi
 fi
 
-# Try to run the setup program
-try_run setup.gtk $args $* 
+# Try to run the setup program - first look for a GTK2 binary
+try_run setup.gtk2 $args $* 
 status=$?
-if [ $status -ne 0 ] && [ $status -ne 2 ] ; then  # setup.gtk couldn't connect to X11 server - ignore
-	try_run setup $args $*
+if [ $status -ne 0 ] && [ $status -ne 2 ] ; then 
+	try_run setup.gtk $args $* 
 	status=$?
-	if [ $status -ne 0 ] && [ $status -ne 2 ]; then
-		echo "The setup program seems to have failed on $arch/$libc"
-		echo
-		echo $FATAL_ERROR
+	if [ $status -ne 0 ] && [ $status -ne 2 ] ; then  # setup.gtk couldn't connect to X11 server - ignore
+		try_run setup $args $*
+		status=$?
+		if [ $status -ne 0 ] && [ $status -ne 2 ]; then
+			echo "The setup program seems to have failed on $arch/$libc"
+			echo
+			echo $FATAL_ERROR
+		fi
 	fi
 fi
 
